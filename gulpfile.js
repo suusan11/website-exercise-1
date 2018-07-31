77cbd55017ef3f850406ce950244ff8b793042a4
git <nismit.dev@gmail.com>
2018-07-30 18:32:51 -0700

    Divide to a task for compiling sass files and add some comments

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,28 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
   
 // style.scssの監視タスクを作成する
+// Watchの場合は別タスクに分けてあげて、別途[タスク名]で指定してあげると動くよ
+// 一度に複数のタスクを実行する場合は ['タスク名', 'タスク名2'] 的な感じで増やせる
 gulp.task('default', function () {
   // ★ style.scssファイルを監視
-  gulp.watch('scss/style.scss', function () {
-    // style.scssの更新があった場合の処理
-  
-    // style.scssファイルを取得
-    gulp.src('scss/style.scss')
-      // Sassのコンパイルを実行
-      .pipe(sass({
-        outputStyle: 'expanded'
-      })
-      // Sassのコンパイルエラーを表示
-      // (これがないと自動的に止まってしまう)
-      .on('error', sass.logError))
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest('scss'));
-  });
+  // ** -> どんな名前のディレクトリでも条件に満たす
+  // *.scss -> 拡張子が.scssで終わるもの全て
+  // scss/**/*.scss -> scssディレクトリ以下の全てのディレクトリかつ.scssで終わるものに変更があった場合
+  // [タスク名] を実行する
+  gulp.watch('scss/**/*.scss',['scss']);
+});
+
+gulp.task('scss', function() {
+  // style.scssファイルを取得
+  gulp.src('scss/**/*.scss')
+  // Sassのコンパイルを実行
+  .pipe(sass({
+    outputStyle: 'expanded'
+  })
+  // Sassのコンパイルエラーを表示
+  // (これがないと自動的に止まってしまう)
+  .on('error', sass.logError))
+  // cssフォルダー以下に保存
+  .pipe(gulp.dest('scss'))
 });
 
