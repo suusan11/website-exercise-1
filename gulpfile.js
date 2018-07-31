d01478cab8d077db0e1d08b640491bfa3347cb90
suusan11 <gawow11@gmail.com>
2018-07-31 15:55:18 -0700

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,29 @@
 const gulp = require('gulp');
 // Sassをコンパイルするプラグインの読み込み
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');  
+const plumber = require("gulp-plumber");
+
   
 // style.scssの監視タスクを作成する
 // Watchの場合は別タスクに分けてあげて、別途[タスク名]で指定してあげると動くよ
 // 一度に複数のタスクを実行する場合は ['タスク名', 'タスク名2'] 的な感じで増やせる
-gulp.task('default', function () {
+gulp.task('default',/*['browser-sync', 'watch'],*/ function () {
   // ★ style.scssファイルを監視
   // ** -> どんな名前のディレクトリでも条件に満たす
   // *.scss -> 拡張子が.scssで終わるもの全て
   // scss/**/*.scss -> scssディレクトリ以下の全てのディレクトリかつ.scssで終わるものに変更があった場合
   // [タスク名] を実行する
   gulp.watch('scss/**/*.scss',['scss']);
+
+  // gulp.watch("./*.html", ['bs-reload']);
+	// gulp.watch("./scss/*.scss", ['bs-reload']);
 });
 
 gulp.task('scss', function() {
   // style.scssファイルを取得
   gulp.src('scss/**/*.scss')
+  .pipe(plumber())
   // Sassのコンパイルを実行
   .pipe(sass({
     outputStyle: 'expanded'
@@ -29,3 +36,17 @@ gulp.task('scss', function() {
   .pipe(gulp.dest('scss'))
 });
 
+//BrowserSync
+gulp.task("browserSyncTask", function () {
+  browserSync({
+      server: {
+          baseDir: "./", // ルートとなるディレクトリを指定
+          index: "index.html" 
+      }
+  });
+
+  //フォルダ以下のファイルを監視
+  gulp.watch("scss/**", function() {
+      browserSync.reload();   // ファイルに変更があれば同期しているブラウザをリロード
+  });
+});
